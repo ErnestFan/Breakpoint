5311c9103cd8cf0099c1e25e84cafc608b434c01
ErnestFan <ernestfan1019@gmail.com>
2018-04-18 17:30:00 -0700

    Add textfield placeholder attribute and shadow on view.

---
+++ breakpoint/View/InsetTextField.swift
@@ -0,0 +1,38 @@
+//
+//  InsetTextField.swift
+//  breakpoint
+//
+//  Created by Ernest Fan on 2018-04-18.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    private var padding = UIEdgeInsetsMake(0, 20, 0, 0)
+    
+    override func awakeFromNib() {
+        setupView()
+        super.awakeFromNib()
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    func setupView() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        
+        self.attributedPlaceholder = placeholder
+    }
+
+}
