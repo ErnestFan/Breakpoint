5405bf9101fcaae181ef666b5f7a1f767190680c
ErnestFan <ernestfan1019@gmail.com>
2018-04-19 22:44:14 -0700

    Build FeedCell.

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by Ernest Fan on 2018-04-19.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
