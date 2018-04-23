2facc14e46642e529dd2defc8c5f6c479e7c2c5f
ErnestFan <ernestfan1019@gmail.com>
2018-04-22 21:01:06 -0700

    Build GroupCell.

---
+++ breakpoint/View/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  breakpoint
+//
+//  Created by Ernest Fan on 2018-04-22.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var groupDescLbl: UILabel!
+    @IBOutlet weak var memberCountLbl: UILabel!
+    
+    func configureCell(title: String, description: String, memberCount: Int) {
+        self.groupTitleLbl.text = title
+        self.groupDescLbl.text = description
+        self.memberCountLbl.text = "\(memberCount) members."
+    }
+    
+}
